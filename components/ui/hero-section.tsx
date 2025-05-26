be7dbcfb8516919a62813c26e2219c5a270548ce
Varian Gustinov <varian.gustinov@MacBooks-MacBook-Pro.local>
2025-05-26 17:18:14 +0700

    fix

--- components/ui/hero-section.tsx
+++ components/ui/hero-section.tsx
@@ -34,7 +34,7 @@ export default function HeroSection({ guestInfo, onOpenInvitation, isOpen }: Her
           className="absolute inset-0 bg-cover bg-center"
           style={{ 
             // backgroundImage: "url('https://images.pexels.com/photos/3532326/pexels-photo-3532326.jpeg?auto=compress&cs=tinysrgb&w=1600')",
-            backgroundImage: "url('/images/latar.png')",
+            backgroundImage: "url('/wedding/images/latar.png')",
           }}
         />
       </div>
